604c190132d015753c4ce2c4f2592bf89e188e59
Raghav Mishra <62120348+raghav07mishra@users.noreply.github.com>
2021-08-06 11:04:06 +0530

    Add files via upload

---
+++ GameWithSticks.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int n,m;
+cin>>n>>m;
+int val=min(n,m);
+if(val%2==0)
+cout<<"Malvika"<<"\n";
+else
+cout<<"Akshat"<<"\n";
+}
